4ceb84673e332139d918471137233acd7640797f
Antoine Busque <abusque@efficios.com>
2015-11-06 00:01:15 -0500

    Add filtering by CPU ID to analyses
    Signed-off-by: Antoine Busque <abusque@efficios.com>

--- lttnganalyses/core/cputop.py
+++ lttnganalyses/core/cputop.py
@@ -86,7 +86,13 @@ class Cputop(Analysis):
     def _process_sched_switch_per_cpu(self, **kwargs):
         timestamp = kwargs['timestamp']
         cpu_id = kwargs['cpu_id']
+        wakee_proc = kwargs['wakee_proc']
         next_tid = kwargs['next_tid']
+
+        if not self._filter_process(wakee_proc):
+            return
+        if not self._filter_cpu(cpu_id):
+            return
 
         if cpu_id not in self.cpus:
             self.cpus[cpu_id] = CpuUsageStats(cpu_id)
@@ -101,10 +107,17 @@ class Cputop(Analysis):
             cpu.current_task_start_ts = timestamp
 
     def _process_sched_switch_per_tid(self, **kwargs):
+        cpu_id = kwargs['cpu_id']
+        wakee_proc = kwargs['wakee_proc']
         timestamp = kwargs['timestamp']
         prev_tid = kwargs['prev_tid']
         next_tid = kwargs['next_tid']
         next_comm = kwargs['next_comm']
+
+        if not self._filter_process(wakee_proc):
+            return
+        if not self._filter_cpu(cpu_id):
+            return
 
         if prev_tid in self.tids:
             prev_proc = self.tids[prev_tid]
@@ -123,8 +136,15 @@ class Cputop(Analysis):
         next_proc.last_sched_ts = timestamp
 
     def _process_sched_migrate_task(self, **kwargs):
+        cpu_id = kwargs['cpu_id']
         proc = kwargs['proc']
         tid = proc.tid
+
+        if not self._filter_process(proc):
+            return
+        if not self._filter_cpu(cpu_id):
+            return
+
         if tid not in self.tids:
             self.tids[tid] = ProcessCpuStats.new_from_process(proc)
 
